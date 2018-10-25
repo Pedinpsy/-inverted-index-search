c3da125709831ea4f72bde76bf8679472fb4076e
Pedro Henrique <pedrocarvalho@nca.ufma.br>
2018-10-25 18:02:45 -0300

    [add] Design parttern strategy to hash method

--- hash.py
+++ hash.py
@@ -1,15 +1,24 @@
+import math
 class content:
 	def __init__(self,key,value):
 		self.value = []
 		self.value.append(value)
 		self.key = key
 
+
+
+
 class hash:
-	def __init__(self,size,limit):
+
+	def __init__(self,size,limit, method="division"):
 		self.array = [None] * size
 		self.size = size
 		self.limit = limit
 		self.count = 0
+		if method == 'multiplication':
+			self.hash = self.getValueMultiplication
+		else:
+			self.hash = self.getValueDivision
 
 	def getArray(self):
 		return self.array
@@ -19,7 +28,7 @@ class hash:
 			return False
 		count = 0
 		while(True):
-			numericKey = self.getNumericKey(key,count)
+			numericKey = self.hash(key,count)
 			print(numericKey)
 			obg = content(key,value)
 			if(self.array[numericKey]!= None):
@@ -42,16 +51,34 @@ class hash:
 		if(len(key)<self.limit):
 			return None
 		count = 0
+		if(self.array[self.hash(key,count)] == None):
+			return None
 		while(True):
-			if(self.array[self.getNumericKey(key,count)].key == key):
-				return self.array[self.getNumericKey(key,count)].value
+
+
+			if(self.array[self.hash(key,count)].key == key):
+				return self.array[self.hash(key,count)].value
 			else:
 				count = count+1
 		return None
 
-	def getNumericKey(self,key,adictionator):
+	def getValueDivision(self,key,adictionator):
 		numericKey = 0
 		for x in range(0,self.limit):
 			numericKey = numericKey + int(ord(key[x]))
 		numericKey = int(numericKey+adictionator%len(self.array))
 		return numericKey
+
+	def getValueMultiplication(self, key, adictionator):
+		adictionator = adictionator+1
+		a = (math.sqrt(5)-1)/2
+		numericKey = 0
+		for x in range(0,self.limit):
+			numericKey = numericKey + int(ord(key[x]))
+
+		numericKey = math.floor(adictionator*((a*numericKey)%1))
+		return numericKey
+
+
+
+
